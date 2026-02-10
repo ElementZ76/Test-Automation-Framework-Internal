da4a5fb24cedc239fef6a84c72904e540f546048
ElementZ76 <pprj2005@gmail.com>
2026-02-10 13:56:38 +0530

    Added method to verify items in cart on CheckoutOveriew Page

--- src/test/java/com/automation/pages/CheckoutOverviewPage.java
+++ src/test/java/com/automation/pages/CheckoutOverviewPage.java
@@ -1,5 +1,8 @@
 package com.automation.pages;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -9,6 +12,12 @@ import com.automation.base.TestBase;
 public class CheckoutOverviewPage extends TestBase {
 	@FindBy(className = "title")
 	WebElement pageTitle;
+	
+	@FindBy(className = "inventory_item_name")
+	List<WebElement> itemNames;
+	
+	@FindBy(className = "cart_item")
+	List<WebElement> orderItems;
 	
 	@FindBy(id = "finish")
 	WebElement finishButton;
@@ -21,7 +30,7 @@ public class CheckoutOverviewPage extends TestBase {
 	 * verify if we reached checkout overview page
 	 * @return true if reached and false if not reached
 	 */
-	public boolean verifyPageTitle() {
+	public boolean isOnOverviewPage() {
 		try {
 			waitForVisibility(pageTitle);
 			return pageTitle.isDisplayed() && pageTitle.getText().equalsIgnoreCase("Checkout: Overview");
@@ -31,8 +40,34 @@ public class CheckoutOverviewPage extends TestBase {
 		}
 	}
 	
-	public void verifyItemsInCart(String productName) {
-		String xpath = "//div[contains(@class='inventory_item_name') and normalize-space()='" + productName +"']";
-		
+	/**
+	 * 
+	 * @return order item names
+	 */
+	public List<String> getOrderItemNames() {
+		return itemNames.stream().map(WebElement::getText).collect(Collectors.toList());
+	}
+	
+	/**
+	 * @return number of items in cart
+	 */
+	public int getOrderItemCount() {
+		return orderItems.size();
+	}
+	
+	/**
+	 * verify order item summary
+	 * @param expectedProducts - expected products from testdata
+	 * @return true or false if matched or unmatched
+	 */
+	public boolean verifyOrderItemSummary(List<String> expectedProducts) {
+		List<String> actualProducts = getOrderItemNames();
+		boolean matches = actualProducts.containsAll(expectedProducts) && actualProducts.size() == expectedProducts.size();
+		if(matches) {
+			log.info("Order summary verified");
+		} else {
+			log.error("Mismatch - Expected = {} and Actual = {}", expectedProducts.size(), actualProducts.size());
+		}
+		return matches;
 	}
 }
