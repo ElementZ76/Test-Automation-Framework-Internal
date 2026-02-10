9095f7033ff1cb9adfd809856e859534ff9de47e
ElementZ76 <pprj2005@gmail.com>
2026-02-10 13:22:40 +0530

    Added method to checkoutoverview page to verify page title

--- src/test/java/com/automation/pages/CheckoutOverviewPage.java
+++ src/test/java/com/automation/pages/CheckoutOverviewPage.java
@@ -1,7 +1,38 @@
 package com.automation.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.automation.base.TestBase;
 
 public class CheckoutOverviewPage extends TestBase {
-
+	@FindBy(className = "title")
+	WebElement pageTitle;
+	
+	@FindBy(id = "finish")
+	WebElement finishButton;
+	
+	public CheckoutOverviewPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	/**
+	 * verify if we reached checkout overview page
+	 * @return true if reached and false if not reached
+	 */
+	public boolean verifyPageTitle() {
+		try {
+			waitForVisibility(pageTitle);
+			return pageTitle.isDisplayed() && pageTitle.getText().equalsIgnoreCase("Checkout: Overview");
+		} catch (Exception e) {
+			log.error("Did not reach checkout overview page.");
+			return false;
+		}
+	}
+	
+	public void verifyItemsInCart(String productName) {
+		String xpath = "//div[contains(@class='inventory_item_name') and normalize-space()='" + productName +"']";
+		
+	}
 }
