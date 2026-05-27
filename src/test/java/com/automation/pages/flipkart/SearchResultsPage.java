ff47754789c2267edc8994fb230e26b7659049db
ElementZ76 <pprj2005@gmail.com>
2026-05-27 12:15:01 +0530

    feat: add methods isLoaded() and hasResults() to verify if SearchResultsPage has load

--- src/test/java/com/automation/pages/flipkart/SearchResultsPage.java
+++ src/test/java/com/automation/pages/flipkart/SearchResultsPage.java
@@ -5,15 +5,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import com.automation.driver.DriverManager.*;
+import java.util.Objects;
 
 import static com.automation.driver.DriverManager.getDriver;
 
-public class SearchResultsPage {
+public class SearchResultsPage extends BasePage {
+
+    @FindBy(xpath = "//span[contains(text(),'Showing') and contains(text(),'results for')]")
+    private WebElement searchResultsText;
 
 
+    public boolean isLoaded() {
+        boolean loaded = Objects.requireNonNull(getDriver().getCurrentUrl()).contains("/search?q=");
+        log.info("Search Results page has loaded successfully");
+        return loaded;
+    }
 
-
+    public int hasResults() {
+        waitForVisibility(searchResultsText);
+        String text = searchResultsText.getText();
+        String total = text.split("of ")[1].split(" results")[0];
+        int count = Integer.parseInt(total.trim());
+        log.info("Search results page is displaying relevant results");
+        return count;
+    }
 
     public SearchResultsPage() {PageFactory.initElements(getDriver(), this);}
 }
